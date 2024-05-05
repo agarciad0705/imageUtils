38a761ef79b439d2c81eaa18ca1095a4804a77d3
Alex Josue Garcia Dominguez <61898231+agarciad0705@users.noreply.github.com>
2024-05-05 02:11:22 -0600

    Update image_color_replace.py
    add aditional description

--- image_color_replace.py
+++ image_color_replace.py
@@ -15,3 +15,20 @@ for item in datas:
  
 img.putdata(newData)
 img.save(sys.argv[2], "PNG")
+
+# JAVA IMPLEMENT
+# String fileUriThumbColor = fileUriThumb.replace("imageFingerprintThumb", "imageFingerprintThumbColor");       
+# StringBuilder commandColor = new StringBuilder("python ").append(scriptColorImage).append(" ")
+#                .append(fileUriThumb).append(" ")
+#                .append(fileUriThumbColor);
+#        log.info("Executing: {}", commandColor.toString());
+#        try {
+#            CommandLine cmdLine = CommandLine.parse(commandColor.toString());
+#            DefaultExecutor defaultExecutor = new DefaultExecutor();
+#            defaultExecutor.execute(cmdLine);
+#            Thread.sleep(500);
+#        } catch (Exception e) {
+#            log.error("Error coloring image with message: {}", e.getMessage());
+#            log.error("Trace: ", e);
+#            fileUriThumbColor = fileUriThumb;
+#        }
